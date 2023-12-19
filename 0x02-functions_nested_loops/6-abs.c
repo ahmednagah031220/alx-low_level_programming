aa5f8bd34698e3401d59be8067cf54c8f4e3d573
Ahmed Nagah <ahmednagah031220@gmail.com>
2023-12-20 01:20:24 +0200

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,11 +5,11 @@
  * @n: the int to check
  * Return: the absolute value of int
  */
-int _abs(int h)
+int _abs(int n)
 {
-	if (h >= 0)
+	if (n >= 0)
 	{
-		return (h);
+		return (n);
 	}
-	return (-h);
+	return (-n);
 }
