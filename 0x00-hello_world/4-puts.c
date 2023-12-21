d5f94aad40a991a82529778482c04957ac0cd4d6
Ahmed Nagah <ahmednagah031220@gmail.com>
2023-12-21 07:05:01 +0200

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 /**
  * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline."
- * return 0.
+ * Return: Always 0.
  */
 
 int main(void)
@@ -12,5 +12,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
