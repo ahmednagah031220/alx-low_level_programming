7db3329d2e1feef794c15af30bfb96b3b36cb6ba
Ahmed Nagah <ahmednagah031220@gmail.com>
2024-04-12 17:07:40 +0200

    task 1 edit

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -5,13 +5,14 @@
  * @str: the string passed to the hash function
  * Return: the key value of the string
  */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
 
 	hash = 5381;
-	while ((c = (*str)++))
+	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33*/
 	}
